5ae9dab1f8b9c714ce2dfc45704886ecc899abc4
ShpakovDmitry <59893335+ShpakovDmitry@users.noreply.github.com>
2020-01-29 21:20:58 +0200

    Deleted compile.sh

--- LCD128_64/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#avr-gcc -S -mmcu=atmega16 -DF_CPU=16000000UL -Os sensor.c && \
-#avrdude -c avrispv2 -p m16 -P /dev/ttyACM0 -U lfuse:w:0xEF:m
-#avrdude -c avrispv2 -p m16 -P /dev/ttyACM0 -U hfuse:w:0xC9:m
-avr-gcc -mmcu=atmega16 -DF_CPU=12000000UL -Os -o LCD.o LCD.c && \
-avr-objcopy -O ihex LCD.o LCD.hex && \
-avrdude -c avrispv2 -p m16 -P /dev/ttyACM0 -U flash:w:LCD.hex
