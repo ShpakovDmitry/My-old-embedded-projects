e885b21f435349ade7e73ee3adad00506dcebe4c
Dmitry_android <work.dmitry.shpakov@gmail.com>
2020-01-30 08:55:35 +0200

    deleted files

--- Embedded/avr_asm/thermometr/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-avr-as main.s -mmcu=attiny13a -o main && \
-avr-ld -o main.elf main && \
-avr-objcopy -O ihex main.elf main.hex && \
-avrdude -c avrispv2 -p m16 -P /dev/ttyACM0 -U flash:w:main.hex
