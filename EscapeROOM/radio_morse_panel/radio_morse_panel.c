6fc86d23c0038fb1c958c5f8b6f03c29586fe41f
Dmitry <work.dmitry.shpakov@gmail.com>
2020-03-15 19:44:40 +0200

    added button RELEASE status mask

--- EscapeROOM/radio_morse_panel/radio_morse_panel.c
+++ EscapeROOM/radio_morse_panel/radio_morse_panel.c
@@ -150,8 +150,17 @@ Button buttons[] = {
 	{ PB7, '8'}
 };
 
-#define ALL_RELEASED (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4) \
-				   | (1 << PB5) | (1 << PB6) | (1 << PB7)
+#define BTN_1_REL (RELEASED << PB0)
+#define BTN_2_REL (RELEASED << PB1)
+#define BTN_3_REL (RELEASED << PB2)
+#define BTN_4_REL (RELEASED << PB3)
+#define BTN_5_REL (RELEASED << PB4)
+#define BTN_6_REL (RELEASED << PB5)
+#define BTN_7_REL (RELEASED << PB6)
+#define BTN_8_REL (RELEASED << PB7)
+
+#define ALL_RELEASED	BTN_1_REL | BTN_2_REL | BTN_3_REL | BTN_4_REL | \
+						BTN_5_REL | BTN_6_REL | BTN_7_REL | BTN_8_REL
 
 uint32_t debounceTime = 100;
 
