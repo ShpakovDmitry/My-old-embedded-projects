aa8ee4d2783765c4bad0a0b6d07996e8e4ea1c3d
Dmitry_work <work.dmitry.shpakov@gmail.com>
2020-04-20 14:31:46 +0300

    using F_CPU define to calculate TIMER0 prescaler and OCRA0 values

--- EscapeROOM/radio_morse_panel/radio_morse_panel.c
+++ EscapeROOM/radio_morse_panel/radio_morse_panel.c
@@ -239,10 +239,19 @@ void initIO(void) {
 void initTimer0(void) {
  	//init TIMER0 in CTC mode, to generate 1ms frequency interrupt, assuming that F_CPU = 16 MHz
 	TCCR0A |= (1 << WGM01);
-	// 1/64 F_CPU prescaler
-	TCCR0B |= (0 << CS02) | (1 << CS01) | (1 << CS00);
+
+	#if ( (F_CPU / 1000) / 64 ) > 0x00ff
+		#define PRESCALER_MASK ( (1 << CS02) | (0 << CS01) | (0 << CS00) )
+		#define OCRA_VALUE ( (F_CPU / 1000) / 128 )
+	#else
+		#define PRESCALER_MASK ( (0 << CS02) | (1 << CS01) | (1 << CS00) )
+		#define OCRA_VALUE ( (F_CPU / 1000) / 64 )
+	#endif
+
+	// F_CPU prescaler
+	TCCR0B |= PRESCALER_MASK;
 	//
-	OCR0A = 250;
+	OCR0A = OCRA_VALUE;
 	// enable output compare A match interrupt
 	TIMSK |= (1 << OCIE0A);
 	// start counting ms at this point; actually when glob interrupt flag is set
