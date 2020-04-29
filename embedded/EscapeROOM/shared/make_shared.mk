4d43f0d074aa868418f47aa0c671c10ec44fdaac
Dmitry_work <work.dmitry.shpakov@gmail.com>
2020-04-29 09:50:32 +0300

    Makefile update

--- embedded/EscapeROOM/shared/make_shared.mk
+++ embedded/EscapeROOM/shared/make_shared.mk
@@ -1,17 +1,23 @@
 CC = avr-gcc
-CCFLAGS += -mmcu=$(MMCU) -DF_CPU=$(F_CPU) $(OPTIMISATION) 
+CCFLAGS += -mmcu=$(MMCU) -DF_CPU=$(F_CPU) $(OPTIMISATION) -Wall 
 BUILD_DIR = build
+SOURCE_DIR = source
+SOURCES = $(wildcard $(SOURCE_DIR)/*.c)
+OBJECTS = $(SOURCES:$(SOURCE_DIR)/%.c=$(BUILD_DIR)/%.o)
 
 all: $(BUILD_DIR) $(BUILD_DIR)/$(TARGET).hex
 
 $(BUILD_DIR):
 	mkdir --parents $(BUILD_DIR)
 
-$(BUILD_DIR)/%.hex: $(BUILD_DIR)/%.o
+$(BUILD_DIR)/$(TARGET).hex: $(BUILD_DIR)/$(TARGET).o
 	avr-objcopy -O ihex $< $@
+
+$(BUILD_DIR)/$(TARGET).o: $(OBJECTS)
+	$(CC) $(CCFLAGS) $(OBJECTS) -o $@ 
 	
-$(BUILD_DIR)/%.o: %.c
-	$(CC) $(CCFLAGS) -o $@ $<
+$(BUILD_DIR)/%.o: $(SOURCE_DIR)/%.c
+	$(CC) $(CCFLAGS) -c $< -o $@
 	
 clean:
 	rm -rf $(BUILD_DIR)
