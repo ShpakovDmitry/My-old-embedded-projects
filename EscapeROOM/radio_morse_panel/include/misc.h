2d46b41ad12e694c26d8c946e0b21548e4099173
Dmitry <work.dmitry.shpakov@gmail.com>
2020-06-01 23:16:45 +0300

    getting jiffies now is atomic opration

--- EscapeROOM/radio_morse_panel/include/misc.h
+++ EscapeROOM/radio_morse_panel/include/misc.h
@@ -8,6 +8,7 @@ bool isOneBitSet (uint8_t x);
 int8_t getSetBitPosition (uint8_t x);
 uint32_t getDeltaTime (uint32_t t1, uint32_t t0);
 void updateButtonState(void);
+uint32_t getJiffies();
 
 #endif // MISC_H
 
