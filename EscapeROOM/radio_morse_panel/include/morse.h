a806013757016755f7d441b1657184100ac6ce30
Dmitry_work <work.dmitry.shpakov@gmail.com>
2020-06-09 16:45:43 +0300

    moved implementation specific routines to source file

--- EscapeROOM/radio_morse_panel/include/morse.h
+++ EscapeROOM/radio_morse_panel/include/morse.h
@@ -3,12 +3,6 @@
 
 #define DOT_DURATION 150
 
-void playUnit();
-void playDot();
-void playDash();
-void playShortSpace();
-void playLetterSpace();
-void playWordSpace();
 char* findMorseChar (char letter);
 void playMorseLetter (char letter);
 void playMorsePhrase (const char* morsePhraseToPlay);
