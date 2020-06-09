168ada0efb4f5d5290ec575cf2f21a8408ba8b94
Dmitry_work <work.dmitry.shpakov@gmail.com>
2020-06-09 16:29:02 +0300

    hiding internal implementation by moving only button relevant information to button.c

--- embedded/EscapeROOM/radio_morse_panel/include/button.h
+++ embedded/EscapeROOM/radio_morse_panel/include/button.h
@@ -3,20 +3,6 @@
 
 #define DEBOUNCE_TIME 100
 
-#define BTN_1_REL (RELEASED << PB0)
-#define BTN_2_REL (RELEASED << PB1)
-#define BTN_3_REL (RELEASED << PB2)
-#define BTN_4_REL (RELEASED << PB3)
-#define BTN_5_REL (RELEASED << PB4)
-#define BTN_6_REL (RELEASED << PB5)
-#define BTN_7_REL (RELEASED << PB6)
-#define BTN_8_REL (RELEASED << PB7)
-
-#define ALL_RELEASED	( BTN_1_REL | BTN_2_REL | BTN_3_REL | BTN_4_REL | \
-						BTN_5_REL | BTN_6_REL | BTN_7_REL | BTN_8_REL )
-
-enum ButtonState { PRESSED, RELEASED };
-
 void updateButtonState(void);
 
 #endif // BUTTON_H
